f5c0f277ca21a3c84c0c031665764fc6b8a3f9be
abdul mannan <muhammadabdulmannan21@gmail.com>
2026-08-28 09:36:37 +0600

    datatypes done

--- datatypes/src/main.rs
+++ datatypes/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    // let x = 2.0; // f64
+
+    // let y: f32 = 3.0; // f32
+    // let f: bool = false;
+    //  let z: char = 'ℤ';
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    let five_hundred = x.0;
+
+    // let a = [1, 2, 3, 4, 5];
+    let a: [i32; 5] = [1, 2, 3, 4, 5];
 }
