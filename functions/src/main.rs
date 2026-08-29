c09c58dad465bf2ff52004376fce56522a7963cd
abdul mannan <muhammadabdulmannan21@gmail.com>
2026-08-29 14:33:51 +0600

    understanding ownership

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -6,3 +6,11 @@ fn main() {
 fn new_function(n: i32) {
     println!("New function with parameter: {n}");
 }
+
+fn function_with_return() -> i32 {
+    5
+}
+
+fn function_with_parameters(x: i32, y: i32) -> i32 {
+    return x + y;
+}
